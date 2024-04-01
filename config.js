fcd2679eea73e58aed4c414accbfc8dcbbf9f36a
Abdel <abdelhakmerabtene@gmail.com>
2024-04-01 02:40:11 +0200

    First commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: 'NX11XRFDHC55HI9YFU81', // TODO
+    secretAccessKey: 'dS9mU72hEpmQSW2UHNs3PQB5FnjUWs3f1fJl4Son', // TODO
+    endpoint: 's3.eu-west-2.wasabisys.com', // TODO
+    bucket: 'esgi2', // TODO
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
